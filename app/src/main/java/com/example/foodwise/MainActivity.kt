fe63bcfb4b7f070211b589ebbcf35ab2d36c8770
rsrulloda <ronell.rulloda@gmail.com>
2023-03-31 16:46:26 -0700

    Updated navigation

--- app/src/main/java/com/example/foodwise/MainActivity.kt
+++ app/src/main/java/com/example/foodwise/MainActivity.kt
@@ -1,92 +1,167 @@
 package com.example.foodwise
 
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import android.os.Bundle
-import androidx.compose.ui.res.vectorResource
-import android.os.Message
-import android.preference.PreferenceActivity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.foodwise.ui.theme.FoodWiseTheme
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.Icon
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.foodwise.ui.theme.FoodWiseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
-        }
-    }
-
-    @Preview
-    @Composable
-    fun MyApp() {
-        val navItems = listOf(
-            NavItem("Camera", Icons.Default.PhotoCamera),
-            NavItem("Inventory", Icons.Filled.List),
-            NavItem("Insight", Icons.Default.Lightbulb)
-        )
-
-        var selectedIndex by remember { mutableStateOf(0) }
-
-        Scaffold(
-            bottomBar = {
-                BottomNavigation{
-                    navItems.forEachIndexed { index, item ->
-                        BottomNavigationItem(
-                            icon = { Icon(item.icon, contentDescription = item.title) },
-                            label = { Text(item.title) },
-                            selected = selectedIndex == index,
-                            onClick = { selectedIndex = index }
+            FoodWiseTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Camera",
+                                    route = "camera",
+                                    icon = Icons.Default.PhotoCamera
+                                ),
+                                BottomNavItem(
+                                    name = "Inventory",
+                                    route = "inventory",
+                                    icon = Icons.Default.List
+                                ),
+                                BottomNavItem(
+                                    name = "Insight",
+                                    route = "insight",
+                                    icon = Icons.Default.Lightbulb
+                                ),
+                            ),
+                            navController = navController,
+                            onItemClick = { navController.navigate(it.route) }
                         )
                     }
-                }
+                ) { Navigation(navController = navController) }
             }
-        ) { innerPadding ->
-            // Content of the screen goes here
-
-            val item1 = Item("Test Item 1", 10.50)
-            val item2 = Item("Test Item 2", 13.50)
-            val item3 = Item("Test Item 3", 9.75)
-
-            val items = mutableListOf(item1, item2, item3)
-
-            LazyColumn {
-                items(items.size) { index ->
-                    Text(items[index].toString(), fontSize = 20.sp)
-                    Divider(color = MaterialTheme.colors.secondary, thickness = 2.dp)
-                }
-            }
-
         }
     }
 
-    data class NavItem(val title: String, val icon: ImageVector)
+    @Composable
+    fun Navigation(navController: NavHostController) {
+        NavHost(navController = navController, startDestination = "inventory") {
+            composable("camera") {
+                CameraScreen()
+            }
+            composable("inventory") {
+                InventoryScreen()
+            }
+            composable("insight") {
+                InsightScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun BottomNavigationBar(
+        items: List<BottomNavItem>,
+        navController: NavController,
+        modifier: Modifier = Modifier,
+        onItemClick: (BottomNavItem) -> Unit
+    ) {
+        val backStackEntry = navController.currentBackStackEntryAsState()
+        BottomNavigation(
+            modifier = modifier,
+            backgroundColor = Color.DarkGray,
+            elevation = 5.dp
+        ) {
+            items.forEach { item ->
+                val selected = item.route == backStackEntry.value?.destination?.route
+                BottomNavigationItem(
+                    selected = selected,
+                    onClick = { onItemClick(item) },
+                    selectedContentColor = Color.White,
+                    unselectedContentColor = Color.Gray,
+                    icon = {
+                        Column(horizontalAlignment = CenterHorizontally) {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                            if (selected) {
+                                Text(
+                                    text = item.name,
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 10.sp
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    fun CameraScreen() {
+        // Content of screen below
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Camera screen")
+        }
+        // Content of screen above
+    }
+
+    @Composable
+    fun InventoryScreen() {
+        // Content of screen below
+        val item1 = Item("Test Item 1", 10.50)
+        val item2 = Item("Test Item 2", 13.50)
+        val item3 = Item("Test Item 3", 9.75)
+
+        val items = mutableListOf(item1, item2, item3)
+
+        LazyColumn {
+            items(items.size) { index ->
+                Text(items[index].toString(), fontSize = 20.sp)
+                Divider(color = MaterialTheme.colors.secondary, thickness = 2.dp)
+            }
+        }
+        // Content of screen above
+    }
+
+    @Composable
+    fun InsightScreen() {
+        // Content of screen below
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Insight screen")
+        }
+        // Content of screen above
+    }
 }
