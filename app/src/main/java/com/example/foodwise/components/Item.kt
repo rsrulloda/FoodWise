69beb45cee711e22f08da0d17a45e6501f468b75
rsrulloda <ronell.rulloda@gmail.com>
2023-04-01 16:13:45 -0700

    Changed Date to LocalDate

--- app/src/main/java/com/example/foodwise/components/Item.kt
+++ app/src/main/java/com/example/foodwise/components/Item.kt
@@ -1,17 +1,20 @@
 package com.example.foodwise.components
 
-import java.util.Date
+import java.time.*
 
 data class Item(
     val description: String,
     val price: Double,
-    val date: Date
+    val date: LocalDate
     ) {
     val getDes: String
         get() = description
 
     val getPrice: Double
         get() = price
+
+    val getDate: LocalDate
+        get() = date
 
     override fun toString(): String {
         return "$description\t\t\t$$price\t\t\t$date"
--- app/src/main/java/com/example/foodwise/ui/screens/InventoryScreen.kt
+++ app/src/main/java/com/example/foodwise/ui/screens/InventoryScreen.kt
@@ -1,31 +1,62 @@
 package com.example.foodwise.ui.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.foodwise.components.Item
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 class InventoryScreen {
+    @RequiresApi(Build.VERSION_CODES.O)
+    val currentDate: LocalDate = LocalDate.now()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     @Composable
     fun Display() {
-        // Content of screen below
-        val item1 = Item("Test Item 1", 10.50, Date(2019))
-        val item2 = Item("Test Item 2", 13.50, Date(2020))
-        val item3 = Item("Test Item 3", 9.75, Date(2021))
 
-        val items = mutableListOf(item1, item2, item3)
+        // Temporary List of Items
+        val item1 = Item("Test Item 1", 10.50, LocalDate.of(2021,4,1))
+        val item2 = Item("Test Item 2", 13.50, LocalDate.of(2022,4,1))
+        val item3 = Item("Test Item 3", 9.75, LocalDate.of(2023,4,1))
+        val item4 = Item("Test Item 4", 9.75, LocalDate.of(2024,4,1))
+        val items = mutableListOf(item1, item2, item3, item4)
 
+        // Scrollable List of Items
         LazyColumn {
             items(items.size) { index ->
-            Text(items[index].toString(), fontSize = 20.sp)
-                Divider(color = MaterialTheme.colors.secondary, thickness = 2.dp)
+                println(ChronoUnit.DAYS.between(currentDate, items[index].getDate)) // Displays how many days until expiration in console
+
+
+                // Checks if item expires within 14 days, if yes then item displayed has red background
+                if(ChronoUnit.DAYS.between(currentDate, items[index].getDate)>14) {
+                    Text("" + items[index] + "| Date Bought: " + items[index].getDate.toString(), fontSize = 20.sp)
+                    Divider(color = Color.Gray, thickness = 2.dp)
+                } else {
+                    Box(modifier = Modifier.background(Color.Red)) {
+                        Text("" + items[index] + "| Date Bought: " + items[index].getDate.toString(), fontSize = 20.sp)
+                        Divider(color = Color.Gray, thickness = 2.dp)
+                    }
+                }
+
+
             }
         }
-        // Content of screen above
     }
 }
