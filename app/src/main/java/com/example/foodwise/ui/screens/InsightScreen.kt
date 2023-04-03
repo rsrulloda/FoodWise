17b1d5df7b2d871c34406ccd51d5f1cea8cd9049
flora <qluo2@horizon.csueastbay.edu>
2023-04-02 17:55:50 -0700

    Tested output of Item class's fourth parameter to track if a food item is wasted or not.

--- app/src/main/java/com/example/foodwise/ui/screens/InsightScreen.kt
+++ app/src/main/java/com/example/foodwise/ui/screens/InsightScreen.kt
@@ -1,8 +1,38 @@
 package com.example.foodwise.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.foodwise.components.Item
+import java.time.LocalDate
 
 class InsightScreen {
     @Composable
-    fun Display() {}
+    fun Display() {
+        var item = Item("Beef", 25.50, LocalDate.of(2021,4,1))
+
+        if(item.getIsWasted) {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Is not wasted" + item.toString())
+            }
+        }
+
+        item.isWasted = true
+        if(item.getIsWasted) {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Text(text = "Wasted:" + item.toString())
+            }
+        }
+    }
 }
